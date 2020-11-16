4600ccb1b8c0266ba7f99338dab8b7d80043508b
JingkunXu <xujingku@msu.edu>
2020-11-15 21:43:39 -0500

    First Update

---
+++ soundboard.js
@@ -0,0 +1,15 @@
+var audio = document.getElementById("firstAudio");
+
+function playFirstAudio(){
+    audio.play();
+}
+
+function playSecondAudio(){
+    let x = document.getElementById("secondAudio")
+    audio.play();
+}
+
+function playThirdAudio(){
+    let x = document.getElementById("thirdAudio")
+    audio.play();
+}
